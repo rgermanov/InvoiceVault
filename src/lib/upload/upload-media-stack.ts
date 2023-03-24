e8b0a24ca273e39e44ab9f953e600410f16b6e36
= <ruslan@germanov.com>
2023-03-24 14:26:41 +0000

    create s3 bucket and sns topic for uploading media

--- src/lib/upload/upload-media-stack.ts
+++ src/lib/upload/upload-media-stack.ts
@@ -1,16 +1,25 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { BlockPublicAccess, Bucket, EventType } from 'aws-cdk-lib/aws-s3';
+import { Topic } from 'aws-cdk-lib/aws-sns';
+import { SnsDestination } from 'aws-cdk-lib/aws-s3-notifications';
 
 export class UploadMediaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucketName = 'upload-media';
+    const topicName = 'upload-media-topic'
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SrcQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const bucket = new Bucket(this, bucketName, {      
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      versioned: true,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL
+    });    
+
+    const topic = new Topic(this, topicName);
+
+    bucket.addEventNotification(EventType.OBJECT_CREATED, new SnsDestination(topic))
   }
 }
