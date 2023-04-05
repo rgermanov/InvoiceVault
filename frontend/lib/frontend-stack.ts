77f91115ca501b633706a5e731ff6845f98b2714
= <ruslan@germanov.com>
2023-04-05 14:22:39 +0000

    add buildspec to the frontend Stack

--- frontend/lib/frontend-stack.ts
+++ frontend/lib/frontend-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as amplify from '@aws-cdk/aws-amplify-alpha';
 import { SecretValue } from 'aws-cdk-lib';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 
 export class FrontendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -14,6 +15,29 @@ export class FrontendStack extends cdk.Stack {
         oauthToken: SecretValue.secretsManager('dev/invoice-vault/github', {
           jsonField: 'invoice-vault-github-token'
         })
+      }),
+      buildSpec: codebuild.BuildSpec.fromObjectToYaml({
+        version: '1.0',
+        frontend: {
+          phases: {
+            preBuild: {
+              commands: [
+                'cd frontend/app',
+                'npm ci',
+                'npm run format'
+              ]
+            },
+            build: {
+              commands: [
+                'npm run build'
+              ]
+            }
+          },
+          artifacts: {
+            baseDirectory: 'frontend/app/dist',
+            files: ["**/*"]
+          }
+        }
       })
     });
 
